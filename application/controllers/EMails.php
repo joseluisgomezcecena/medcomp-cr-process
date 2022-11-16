c00d1b8ef8a30f64b3221a9bc1da7c8ce7fe89e5
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2022-11-15 16:48:13 -0800

    Updated email tests controller.

--- application/controllers/EMails.php
+++ application/controllers/EMails.php
@@ -6,14 +6,14 @@ class  EMails extends  CI_Controller
 		$this->load->library('email');
 
 		$subject = 'Test';
-		$message = "Test data";
-		$message .= "<p><br><br><br><br><br/>Este es un correo automatizado de la aplicación <b>Moulding Tool App.</b></p>";
+		$message = "Test data...<br>Please confirm if you received this email.";
+		$message .= "<p><br><br><br><br><br/>This is an automated test from <b>Change Request Application.</b></p>";
 
 		$result = $this->email
 			->from('jgomez@martechmedical.com')
 			->reply_to('noreply@martechmedical.com')    // Optional, an account where a human being reads.
 			//->to('jgomez@martechmedical.com')
-			->to('jgomez@martechmedical.com')
+			->to('jgomez@martechmedical.com,dcheng@medcompnet.com,dwitt@medcompnet.com')
 			->subject($subject)
 			->message($message)
 			->send();
@@ -24,6 +24,4 @@ class  EMails extends  CI_Controller
 		//exit;
 		//email test end
 	}
-
-
 }
